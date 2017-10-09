4e45977b1c45cde378193edff0c2e5e71345d65f
Joshua Lawson <jacklegjosh@gmail.com>
2017-10-09 06:23:25 -0400

    functionality complete

--- oregano-trail.js
+++ oregano-trail.js
@@ -6,15 +6,15 @@
 
 // The following code should work once you are finished:
 
-// let wagon = makeWagon(5);
-// let traveler = makeTraveler('Henrietta');
-// let traveler2 = makeTraveler('Juan');
+// x let wagon = makeWagon(5);
+// x let traveler = makeTraveler('Henrietta');
+// x let traveler2 = makeTraveler('Juan');
 
-// hunt(traveler); // maybe get more food ****
-// eat(traveler2);
-// eat(traveler2); // juan is hungry
-// join(wagon, traveler);
-// join(wagon, traveler2);
+// x hunt(traveler); // maybe get more food
+// x eat(traveler2);
+// ? eat(traveler2); // juan is hungry
+// x join(wagon, traveler);
+// x join(wagon, traveler2);
 
 // console.log(quarantine(wagon)); // print true if someone is sick, false otherwise
 // console.log(food(wagon)); // print juan's food + henrietta's food
@@ -39,16 +39,18 @@
 // eat(traveler)
 // Consumes 20 of the traveler's food. If the traveler doesn't have 20 food, the traveler is no longer healthy.
 
-// join(wagon, traveler)
+// x join(wagon, traveler)
 // Add the traveler to the wagon if there is space. If there is not enough capacity, don't add them.
 
-// quarantine(wagon)
+// x quarantine(wagon)
 // Return true if there is at least one unhealthy person in the wagon. Return false if not.
 
 // food(wagon)
 // Return the total amount of food among all occupants of the wagon.
 
 window.addEventListener('load', function() {
+    let start = document.querySelector('button');
+    start.addEventListener ('click', function() {
 
     function makeTraveler(name) {
         return {
@@ -91,15 +93,53 @@ window.addEventListener('load', function() {
     function eat(person) {
         console.log("Eatin' time!");
         if (person.food > 0) {
-        person.food -= 25;
-        console.log(person.name + "'s food -25");
+        person.food -= 20;
+        console.log(person.name + "'s food -20");
         } else {
             console.log(person.name + " doesn't have any food!")
         }
     };
 
+    function join(wagon, person) {
+        if (wagon.capacity > wagon.manifesto.length) {
+            wagon.manifesto.push(person);
+        } else {
+            console.log("There's no room on this wagon!");
+        }
+    }
+
+    // true is not displaying. Is that okay?
+    function quarantine(wagon) {
+        for (let i = 0; i < wagon.manifesto.length; i++) {
+            if (wagon.manifesto[i].isHealthy === true) {
+                console.log("No need, everyone is healthy.");
+                return false;
+            } else {
+                console.log(wagon.manifesto[i].name + " is sick!");
+                return true;
+            };
+        };
+    }
+
+    function food(wagon) {
+        let totalFood = 0;
+        for (let i = 0; i < wagon.manifesto.length; i++) {
+            totalFood += wagon.manifesto[i].food;
+            console.log(totalFood);
+        }
+    }
+
     hunt(traveler);
-    console.log(traveler);
+    hunt(traveler2);
+    console.log(traveler, traveler2);
     eat(traveler);
     console.log(traveler);
+    join(wagon, traveler);
+    console.log(wagon);
+    join(wagon, traveler2);
+    console.log(wagon);
+    traveler.isHealthy = false;
+    quarantine(wagon);
+    food(wagon);
+    });
 })
